a0f1e14b3ebb72ed1ff7d943486d93c86ae6ef90
Johongirr <johongir_rahimov@bk.ru>
2021-07-28 11:39:06 +0200

    made it work

---
+++ index.js
@@ -0,0 +1,73 @@
+const images = document.querySelectorAll("img");
+const circles = document.querySelectorAll(".circle");
+const imgContainer = document.querySelector(".images");
+const sliderLeftBtn = document.querySelector(".slider-left");
+const sliderRightBtn = document.querySelector(".slider-right");
+
+let counter = 0;
+const imgWidth = images[0].width;
+let intervalId;
+
+const removeInterval = () => {
+  clearInterval(intervalId);
+};
+const addInterval = () => {
+  intervalId = setInterval(() => {
+    slideToRight();
+  }, 5000);
+};
+const removeClassFromCircle = () => {
+  circles.forEach((circle) => circle.classList.remove("circle--active"));
+};
+const addClassToCircle = (index) => {
+  circles[index].classList.add("circle--active");
+};
+const slideImgInInterval = (yes = true) => {
+  if (yes) {
+    removeInterval();
+    addInterval();
+  }
+};
+slideImgInInterval();
+
+const slideToLeft = () => {
+  counter--;
+  if (counter < 0) {
+    counter = 4;
+  }
+  imgContainer.style.transform = `translateX(-${counter * imgWidth}px)`;
+  removeClassFromCircle();
+  addClassToCircle(counter);
+  slideImgInInterval(true);
+};
+const slideToRight = () => {
+  counter++;
+  if (counter >= 5) {
+    counter = 0;
+  }
+  imgContainer.style.transform = `translateX(-${counter * imgWidth}px)`;
+  removeClassFromCircle();
+  addClassToCircle(counter);
+  slideImgInInterval(true);
+};
+const getActiveCircleIndex = () => {
+  return Array.from(circles).findIndex((circle) =>
+    circle.classList.contains("circle--active")
+  );
+};
+const slideImg = (e) => {
+  const activeCircleIndex = getActiveCircleIndex();
+  const currCircleIndex = parseInt(e.target.dataset.index);
+  slideImgInInterval(true);
+  if (currCircleIndex >= activeCircleIndex) {
+    counter = currCircleIndex - 1;
+    slideToRight();
+  } else {
+    counter = currCircleIndex + 1;
+    slideToLeft();
+  }
+};
+
+sliderLeftBtn.addEventListener("click", slideToLeft);
+sliderRightBtn.addEventListener("click", slideToRight);
+circles.forEach((circle) => circle.addEventListener("click", slideImg));
